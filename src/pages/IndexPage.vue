4deca9b05ad3d53b059dcd489d63f897584e0cb8
JinJoa <36723194+JinJoa@users.noreply.github.com>
2023-08-12 17:04:11 +0900

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="column flex-center">
+    <div>
+      <q-btn
+        @click="onClickTimer()"
+        label="스탑와치"
+        color="green"
+        icon="timer"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+const $router = useRouter();
+
+const count = ref(0);
+
+const min = -5;
+const max = 5;
+
+const onClickTimer = () => {
+  //alert('timer');
+  console.log('onClickTimer');
+  $router.push('/stopwatch');
+};
+</script>
